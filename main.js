54e80c1149314533c2f80088ae6db32d58c737e8
ruth-create <80636279+ruth-create@users.noreply.github.com>
2021-10-18 18:53:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var timer_counter=0;
+var timer_check= null;
+var drawn_sketch= null;
+var answer_holder= null;
+var score=0;
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    background("white");
+}
+
+function clearCanvas(){
+    backround("white");
+}
